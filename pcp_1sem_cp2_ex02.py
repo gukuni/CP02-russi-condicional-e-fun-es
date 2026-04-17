30df85d58837f9cb87928971ad3b6c25e26826b4
pFerreras0203 <pedro.ferreras8@gmail.com>
2026-04-16 21:55:57 -0300

    exercicio 2 finalizado

--- pcp_1sem_cp2_ex02.py
+++ pcp_1sem_cp2_ex02.py
@@ -1,1 +1,29 @@
+a = float(input("Digite o lado A: "))
+b = float(input("Digite o lado B: "))
+c = float(input("Digite o lado C: "))
 
+if a >= b and a >= c:
+    A = a
+    B, C = max(b, c), min(b, c)
+elif b >= a and b >= c:
+    A = b
+    B, C = max(a, c), min(a, c)
+else:
+    A = c
+    B, C = max(a, b), min(a, b)
+
+if A >= B + C:
+    print("não forma triangulo")
+else:
+
+    if A**2 == B**2 + C**2:
+        print("triangulo retangulo")
+    elif A**2 > B**2 + C**2:
+        print("triangulo obtusangulo")
+    else:
+        print("triangulo acutangulo")
+
+    if A == B == C:
+        print("trinagulo equilatero")
+    elif A == B or B == C or A == C:
+        print("triangulo isosceles")
