2284457efcbc9eab3d8d0b74cf280603c598159d
victorcbinot <victorcbinot@gmail.com>
2026-04-24 09:09:45 -0300

    Exercício 1 arrumado

--- pcp_1sem_cp2_ex01.py
+++ pcp_1sem_cp2_ex01.py
@@ -5,22 +5,27 @@ conversao = peso_da_carga * 1000
 
 
 if codigo_da_carga >= 10 and codigo_da_carga <= 20:
-   calculo = 100 * conversao
+   preco_da_carga = 100 * conversao
 elif codigo_da_carga >= 21 and codigo_da_carga <= 30:
-   calculo = 250 * conversao
+   preco_da_carga = 250 * conversao
 else:
-   calculo = 400 * conversao
+   preco_da_carga = 340 * conversao
 
 if codigo_do_estado == 1:
-   valor_imposto = 1.35 * calculo
+   valor_imposto = 0.35 * preco_da_carga
 elif codigo_do_estado == 2:
-   valor_imposto = 1.25 * calculo
+   valor_imposto = 0.25 * preco_da_carga
 elif codigo_do_estado == 3:
-   valor_imposto = 1.15 * calculo
+   valor_imposto = 0.15 * preco_da_carga
 elif codigo_do_estado == 4:
-   valor_imposto = 1.05 * calculo
+   valor_imposto = 0.05 * preco_da_carga
 else:
-   valor_imposto = calculo
+   valor_imposto = 0 * preco_da_carga
 
-print(f"Valor total transportado pelo caminhão é: {valor_imposto}")
+valor_total = preco_da_carga + valor_imposto
+
+
+
+print(f"Valor total transportado pelo caminhão é: {valor_total:.1f}")
+
 
