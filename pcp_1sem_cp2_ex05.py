f215c851dd0e1be77580c878fab6288cb955fcf8
gufiapexe <guhkunitaki@gmail.com>
2026-04-24 10:57:33 -0300

    Correção do exercício

--- pcp_1sem_cp2_ex05.py
+++ pcp_1sem_cp2_ex05.py
@@ -2,24 +2,31 @@
 
 
 def juros_parcela(parcela):
-    if parcela <= 6:
+    if 3 <= parcela <= 6:
         return 0.05
-    elif parcela <= 12:
+    elif 7 <= parcela <= 12:
         return 0.08
-    elif parcela <= 24:
+    elif 13 <= parcela <= 24:
         return 0.10
+     
 def calculo_pmt(emprestimo, parcela):
     i = juros_parcela(parcela)
     pmt = emprestimo * (i * (1 + i)**parcela) / ((1 + i)**parcela - 1)
     return pmt
 
 
+def pode_aprovar(idade, emprestimo, renda_mensal):
+    return idade >= 18 and emprestimo <= renda_mensal *20
 
-def idade_permitida(idade):
-    return idade >= 18
 
-def parcela_permitida(emprestimo, renda_mensal):
-    return emprestimo <= renda_mensal *20
+
+def valor_total (pmt, parcela):
+    total = pmt * parcela
+    return total
+
+def valor_juros (total, emprestimo):
+    juros = total - emprestimo
+    return juros
 
 
 cliente = input("Digite o nome do cliente: ").strip()
@@ -27,15 +34,19 @@ idade = int(input("Digite a sua idade: "))
 renda_mensal = float(input("Digite o valor da renda mensal: "))
 emprestimo = float(input("Qual o valor desejado do emprestimo? "))
 parcela = int(input("Você quer fazer em quantas parcelas (3 até 24)?"))
+while parcela < 3 or parcela > 24:
+    parcela = int(input("Número de parcelas inválidos! Digite o número de parcelas novamente: "))
 if idade_permitida(idade) and parcela_permitida(emprestimo, renda_mensal):
     taxa = juros_parcela(parcela) * 100
     pmt = calculo_pmt(emprestimo, parcela)
+    total = valor_total(pmt, parcela)
+    juros = valor_juros(total, emprestimo)
     print('Emprestimo aprovado!')
     print(cliente)
     print(emprestimo)
     print(f'{taxa}%')
     print(f'{pmt:.2f}')
-    print(f'{pmt * parcela:.2f}')
-    print(f'{(pmt * parcela) - emprestimo:.2f}')
+    print(f'{total:.2f}')
+    print(f'{juros:.2f}')
 else:
     print('Empréstimo negado!')
